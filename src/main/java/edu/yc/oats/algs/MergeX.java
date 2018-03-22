863ee8704ae2f55247affd144ed00f8c96a4f898
avraham <avrahamleff@gmail.com>
2018-03-22 16:37:19 +0000

    Copied interface, enum, static methods "as is" from my version, added stripped down versions of my implementation classes.

---
+++ src/main/java/edu/yc/oats/algs/MergeX.java
@@ -0,0 +1,12 @@
+package edu.yc.oats.algs;
+
+public class MergeX implements Mergesorter {
+
+  MergeX() {
+  }
+
+  @SuppressWarnings("unchecked")
+  public void sortIt(Comparable[] a) {
+      // fill me in
+  }  
+}
---
+++ src/main/java/edu/yc/oats/algs/ParallelMerge.java
@@ -0,0 +1,12 @@
+package edu.yc.oats.algs;
+
+public class ParallelMerge implements Mergesorter {
+
+  ParallelMerge() {
+  }
+
+  @SuppressWarnings("unchecked")
+  public void sortIt(Comparable[] a) {
+      // fill me in
+  }  
+}
